e95426f2497b469c8664adf20f108aba89335af3
ElohegyiI <roxnadz78@gmail.com>
2022-02-16 08:17:25 +0100

    started

---
+++ frontend/script.js
@@ -0,0 +1,24 @@
+let sentence1 = ["Basketball is the best sport ever"]
+let sentence2 = ["I have never seen this before"]
+let sentence3 = ["No problems have been detected in the workplace"]
+let sentence4 = ["returns the sentence it was given"]
+let sentence5 = ["write a function that uses two parameters"]
+
+function cleanse(sentence, words) {
+	let mySplit = sentence.split("");
+	let result = [""]
+
+	for ( let i of sentence1)
+	let isCleanWord = true;
+
+	for ( let n of words)
+	let isCleanWord = false; 
+			if(typeof "" === "")
+			return ...
+				if(typeof "isCleanWord" === true) {
+					return result + words
+				} else {
+					return result + "*****"
+				}
+
+}
