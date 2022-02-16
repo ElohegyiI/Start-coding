90eda36304ab4e31219ea2e2d3a502d44bbfcb4d
ElohegyiI <roxnadz78@gmail.com>
2022-02-16 08:35:56 +0100

    started

--- frontend/script.js
+++ frontend/script.js
@@ -1,24 +1,6 @@
-let sentence1 = ["Basketball is the best sport ever"]
-let sentence2 = ["I have never seen this before"]
-let sentence3 = ["No problems have been detected in the workplace"]
-let sentence4 = ["returns the sentence it was given"]
-let sentence5 = ["write a function that uses two parameters"]
+let doors = [""]
 
-function cleanse(sentence, words) {
-	let mySplit = sentence.split("");
-	let result = [""]
+for (let i = 1; i <= 100;);
+	isOpen = {} + doorNumber = {};
 
-	for ( let i of sentence1)
-	let isCleanWord = true;
-
-	for ( let n of words)
-	let isCleanWord = false; 
-			if(typeof "" === "")
-			return ...
-				if(typeof "isCleanWord" === true) {
-					return result + words
-				} else {
-					return result + "*****"
-				}
-
-}
+	for (let i = 1; i <= 100;);
