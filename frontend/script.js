4e8361af9b46e81a770e582d63ec6487efd84a83
ElohegyiI <roxnadz78@gmail.com>
2022-02-17 10:46:19 +0100

    update

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,22 @@
 let doors = [];
 
 for (let i = 1; i <= 100;) {
-	doors[i]
+	doors[i] = {
+		"doorNumber": i + 1,
+		"isOpen": false
+	}
 }
-	isOpen = {} + doorNumber = {};
-
-	for (let i = 1; i <= 100;);
+for (let i = 1; let <= 100;) {
+	for (const door of doors) {
+		let shouldToggle = door.doorNumber % (i + 1);
+			if (shouldToggle = true) {
+				door.isOpen = !doorisOpen;
+			}
+		
+	}
+}
+for (doors of doors) {
+	if (doors.isOpen === true) {
+		console.log(doors.doorNumber);
+	}
+}	
