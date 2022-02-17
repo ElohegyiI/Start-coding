2bd37f005c7912ddb563775653e048039bf7d1d9
ElohegyiI <roxnadz78@gmail.com>
2022-02-17 12:25:27 +0100

    update

--- frontend/script.js
+++ frontend/script.js
@@ -2,36 +2,36 @@ let bankAccount1 = {
 	"id": 324020,
 	"name": "Pista",
 	"balance": 10000,
-	"transaction": {
+	"transaction": [{
 		"amount": 500,
 		"isSuccesfull": true,
 		"type": "deposit",
 		"date": "2022.02.10"
-	}
+	}]
 }
 
 let bankAccount2 = {
 	"id": 344121,
 	"name": "Jóska",
 	"balance": 25000,
-	"transaction": {
+	"transaction": [{
 		"amount": 1000,
 		"isSuccesfull": true,
 		"type": "deposit",
 		"date": "2022.02.01"
-	}
+	}]
 }
 
 let bankAccount3 = {
 	"id": 356789,
 	"name": "Kriszti",
 	"balance": 12500,
-	"transaction": {
+	"transaction": [{
 		"amount": 1530,
 		"isSuccesfull": true,
 		"type": "withdraw",
 		"date": "2022.02.06"
-	}
+	}]
 }
 
 
@@ -45,8 +45,8 @@ let bankAccounts = {
 function getDateOfHighestDeposit(account) {
     let dateOfHighest = null;
     let amountOfHighest = 0;
-}
-	for (transaction of bankAccounts.transaction) {
+};
+	for (transactions of bankAccounts.transaction) {
 		if(transaction.type === "deposit" && transaction.isSuccesfull === true) {
 			let isRelevant = true;
 			if (isRelevant === true && transaction.amount > amountOfHighest) {
