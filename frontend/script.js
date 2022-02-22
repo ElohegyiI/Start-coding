f97d6920da7f35a860e8d7d3c2423d83021ce57b
ElohegyiI <roxnadz78@gmail.com>
2022-02-22 17:32:36 +0100

    update

--- frontend/script.js
+++ frontend/script.js
@@ -1,28 +1,39 @@
 let sentence1 = ["Basketball is the best sport ever"]
-let sentence2 = ["I have never seen this before"]
+/*let sentence2 = ["I have never seen this before"]
 let sentence3 = ["No problems have been detected in the workplace"]
 let sentence4 = ["returns the sentence it was given"]
 let sentence5 = ["write a function that uses two parameters"]
 
-let wordChanged = ["best", "seen", "been", "was", "uses"]
+let wordChanged = ["best", "seen", "been", "was", "uses"]*/
 
 function cleanse(sentence, words) {
-	let result = "";
-	let mySplit = sentence.split(" ");
+	let result = ""
+	
+	let wordsFromSentence = sentence.split(" ")
+	
 
-	for (const word of mySplit) {
+	for (const wordFromSentence of wordsFromSentence) {
 		let isCleanWord = true;
-		for (const myWords of words) {
-			if(wordChanged === word) {
-				let isCleanWord = false;
+		for (const word of words) {
+			if(word === wordFromSentence) {
+				isCleanWord = false;
 			}			
 		}
-	}
-	if (isCleanWord = true) {
-		result = result + words + ""
-	} else {
-		result = result + "****"
+		if (isCleanWord === true) {
+			result += `${wordFromSentence}`
+		} else {
+			let uglyWord = ""
+			for (let i = 0; i < wordFromSentence.length; i++) {
+				uglyWord += "*"
+				
+			}
+			result += `${uglyWord}`
+		}
+		return result
 	}
 
+
+	
+
 }
-console.log(cleanse(sentence1))
+console.log(cleanse(sentence1, ['Basketball', 'best']))
