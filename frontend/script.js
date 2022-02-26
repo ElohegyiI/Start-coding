5cb0f3de897cc1c0cc948e2648b47e7c2e130712
ElohegyiI <roxnadz78@gmail.com>
2022-02-26 10:29:51 +0100

    update

--- frontend/script.js
+++ frontend/script.js
@@ -4,11 +4,17 @@ let bankAccount1 = {
 	"id": 324020,
 	"name": "Pista",
 	"balance": 10000,
-	"transaction": [{
+	"transactions": [{
 		amount: 500,
 		isSuccesfull: true,
 		type: "deposit",
-		date: "2022.02.10"
+		date: "2022.02.01"
+	},
+	{
+		amount: 1500,
+		isSuccesfull: true,
+		type: "withdraw",
+		date: "2022.04"
 	}]
 }
 
@@ -16,11 +22,18 @@ let bankAccount2 = {
 	"id": 344121,
 	"name": "Jóska",
 	"balance": 25000,
-	"transaction": [{
+	"transactions": [{
 		amount: 1000,
 		isSuccesfull: true,
 		type: "deposit",
-		date: "2022.02.01"
+		date: "2022.02.10"
+	},
+	{
+		amount: 2000,
+		isSuccesfull: true,
+		type: "deposit",
+		date: "2022.02.09"
+
 	}]
 }
 
@@ -28,11 +41,17 @@ let bankAccount3 = {
 	"id": 356789,
 	"name": "Kriszti",
 	"balance": 12500,
-	"transaction": [{
+	"transactions": [{
 		amount: 1530,
 		isSuccesfull: true,
 		type: "withdraw",
 		date: "2022.02.06"
+	},
+	{
+		amount: 2530,
+		isSuccesfull: true,
+		type: "withdraw",
+		date: "2022.02.08"
 	}]
 }
 
@@ -48,17 +67,24 @@ function getDateOfHighestDeposit(account) {
     let dateOfHighest = null;
     let amountOfHighest = 0;
 
-	for (transaction of account.transaction) {
+	for (transaction of account.transactions) {
+	
+		let isRelevant = false;
+		
 		if(transaction.type === "deposit" && transaction.isSuccesfull === true) {
-			let isRelevant = true;
-			if (isRelevant === true && transaction.amount > amountOfHighest) {
-				amountOfHighest = transaction.amount;
-                dateOfHighest = transaction.date;
-			}
+			isRelevant = true;
+			
+			
 		}
-	return dateOfHighest
+		if (isRelevant === true && transaction.amount > amountOfHighest) {
+			amountOfHighest = transaction.amount;
+			dateOfHighest = transaction.date;
+			
+			}
 	}
+	return dateOfHighest
+	
 	/*return bankAccount3.transaction*/
 }	
-console.log( getDateOfHighestDeposit(bankAccount1) )
+console.log( getDateOfHighestDeposit(bankAccount2) )
 	
