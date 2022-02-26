af4a9da9c3bdccefaaf8a767df817696a12db60b
ElohegyiI <roxnadz78@gmail.com>
2022-02-26 08:57:42 +0100

    update

--- frontend/script.js
+++ frontend/script.js
@@ -4,28 +4,29 @@ function getOpenedDoors() {
 
 	for (let i = 1; i <= 100; i++) {		
 	        doors.push({
-	            "isOpen": false,
-	            "doorNumber": i
+	            isOpen: false,
+	            doorNumber: i
 			})
 		
 	}
-	/*console.log(doors)*/
+	//console.log(doors)
 	for (let i = 1;  i <= 100; i++) {
 		for (const door of doors) {
 			let shouldToggle = false;
 			if (door.doorNumber % i === 0)  {
-				shouldToggle === true; 
+				shouldToggle = true; 
 
 			}
 			if (shouldToggle === true) {
 				door.isOpen = !door.isOpen;							
+			}
 		}
-	}
+
 		for (const door of doors) {
 			if (door.isOpen === true) {
 			console.log(door.doorNumber);
+			}
 		}
-	}
 	}
 }
 getOpenedDoors()
